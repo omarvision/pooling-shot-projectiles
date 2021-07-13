f7988a028b241e4089837fc08297b9321313686e
Omar Santiago <omarkenosia@gmail.com>
2021-07-13 10:22:17 -0400

    Add files via upload

---
+++ CubePlayer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePlayer : MonoBehaviour
+{
+    public float movespeed = 3.5f;
+    public float shootspeed = 0.5f;
+    public float xTravel = 5.0f;
+    private CubeShot[] cubeshots;
+    private float markTime;
+
+    private void Start()
+    {
+        markTime = Time.time;
+        cubeshots = GameObject.Find("ShotPool").GetComponentsInChildren<CubeShot>();
+    }
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") == true || Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            doShoot();
+        }
+
+        float xDelta = Input.GetAxis("Horizontal") * movespeed * Time.deltaTime;
+        float x = this.transform.position.x;
+        if ((xDelta > 0 && x < xTravel) || (xDelta < 0 && x > -xTravel))
+        {
+            this.transform.Translate(new Vector3(xDelta, 0, 0));
+        }
+
+    }
+    private void doShoot()
+    {
+        bool bFound = false;
+        if (Time.time - markTime > shootspeed)
+        {
+            markTime = Time.time;
+            for (int i = 0; i < cubeshots.Length; i++)
+            {
+                CubeShot script = cubeshots[i].GetComponent<CubeShot>();
+                if (script.setActive() == true)
+                {
+                    bFound = true;
+                    break;
+                }
+            }
+            if (bFound == false)
+            {
+                this.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
---
+++ CubeShot.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeShot : MonoBehaviour
+{
+    public enum enumState
+    {
+        pooled,
+        active
+    }
+
+    public float movespeed = 3.0f;
+    public float zTravel = 5.0f;
+    private enumState state = enumState.pooled;
+    private GameObject player = null;
+
+    private void Start()
+    {
+        player = GameObject.Find("CubePlayer");
+    }
+    private void Update()
+    {
+        switch (state)
+        {
+            case enumState.pooled:
+                break;
+            case enumState.active:
+                this.transform.Translate(new Vector3(0, 0, movespeed * Time.deltaTime));
+                if (this.transform.position.z > zTravel)
+                    setPooled();
+                break;
+        }
+    }
+    private void setPooled()
+    {
+        state = enumState.pooled;
+        this.GetComponent<Renderer>().enabled = false;
+    }
+    public bool setActive()
+    {
+        bool ret = false;
+        if (state == enumState.pooled)
+        {
+            state = enumState.active;
+            this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+            this.transform.rotation = new Quaternion(player.transform.rotation.x, player.transform.rotation.y, player.transform.rotation.z, player.transform.rotation.w);
+            this.GetComponent<Renderer>().enabled = true;
+            this.GetComponent<AudioSource>().Play();
+            ret = true;
+        }
+        return ret;
+    }
+}
